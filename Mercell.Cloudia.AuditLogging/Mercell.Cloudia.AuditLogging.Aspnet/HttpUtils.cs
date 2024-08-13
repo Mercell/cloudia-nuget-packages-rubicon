04271f80cd4dddbb0972cf50eb33db400834e3ae
jtjuslin <jarkko.juslin@gmail.com>
2024-08-13 17:11:07 +0300

    RUB-12164 add some documentation for the code

--- Mercell.Cloudia.AuditLogging/Mercell.Cloudia.AuditLogging.Aspnet/HttpUtils.cs
+++ Mercell.Cloudia.AuditLogging/Mercell.Cloudia.AuditLogging.Aspnet/HttpUtils.cs
@@ -11,6 +11,17 @@ namespace Mercell.Cloudia.AuditLogging.Aspnet
 {
 
 #if NET461_OR_GREATER
+
+    /// <summary>
+    /// Http Module to ensure that values set in the audit logging context do not mix between requests.
+    /// 
+    /// Hooks into the Application_BeginRequest to initialize the context with default values.
+    /// * Sets correlation id to a new Guid value.
+    /// * Sets ssid to an empty Guid value (all zeroes).
+    /// 
+    /// Hooks into the Application_EndRequest to clear the context.
+    /// * Does exactly the same as the begin request -hook.
+    /// </summary>
     public class AuditLoggerHttpModule : IHttpModule
     {
         public void Init(HttpApplication application)
@@ -18,10 +29,10 @@ namespace Mercell.Cloudia.AuditLogging.Aspnet
             application.BeginRequest += new EventHandler(Application_BeginRequest);
             application.EndRequest += new EventHandler(Application_EndRequest);
         }
-
+        
         private void Application_BeginRequest(object source, EventArgs e)
         {
-            AuditLoggerContext.Reset();
+            AuditLoggerContext.Initialize();
         }
 
         private void Application_EndRequest(object source, EventArgs e)
@@ -34,6 +45,16 @@ namespace Mercell.Cloudia.AuditLogging.Aspnet
     
 #elif NETSTANDARD2_0_OR_GREATER
 
+    /// <summary>
+    /// Middleware to ensure that values set in the audit logging context do not mix between requests.
+    /// 
+    /// Upon invocation
+    /// * Sets correlation id to a new Guid value.
+    /// * Sets ssid to an empty Guid value (all zeroes).
+    /// 
+    /// Afterwards
+    /// * Does exactly the same as the begin request -hook.
+    /// </summary>
     public class AuditLoggerMiddleware
     {
         private readonly RequestDelegate _next;
@@ -45,7 +66,7 @@ namespace Mercell.Cloudia.AuditLogging.Aspnet
 
         public async Task InvokeAsync(HttpContext context)
         {
-            AuditLoggerContext.Reset();
+            AuditLoggerContext.Initialize();
             try
             {
                 await _next(context);
